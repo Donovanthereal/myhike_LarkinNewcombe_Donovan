9466ba37a00b00180eb3b69e7849e717a793ff83
Donovan Larkin Newcombe <2015donovan@gmail.com>
2025-10-28 11:52:08 -0700

    Images 3 + 4

--- src/main.js
+++ src/main.js
@@ -61,6 +61,9 @@ async function displayCardsDynamically() {
             newcard.querySelector('.card-text').textContent = hike.details || `Located in ${hike.city}.`;
             newcard.querySelector('.card-length').textContent = hike.length;
 
+            // 👇 ADD THIS LINE TO SET THE IMAGE SOURCE
+            newcard.querySelector('.card-image').src = `./images/${hike.code}.jpg`;
+
             // Attach the new card to the container
             document.getElementById("hikes-go-here").appendChild(newcard);
         });
